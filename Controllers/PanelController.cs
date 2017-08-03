a63206cab7fbcf06ee9799835d5c96a3eefdf606
Pato Perez <pato12p@gmail.com>
2017-08-03 13:33:06 -0300

    Configurar router de angular y documentación para ejecutar

--- Controllers/PanelController.cs
+++ Controllers/PanelController.cs
@@ -10,58 +10,11 @@ namespace PriHood.Controllers
   {
     public IActionResult Index()
     {
-      return View();
-    }
 
-    public IActionResult Comunicaciones()
-    {
-      return View();
-    }
-
-    public IActionResult VerComunicacion(string index)
-    {
-      return View();
-    }
-
-    [HttpGet]
-    public IActionResult Usuarios(string index, int rowId)
-    {
-      string[] Residencias = { "RES 1", "RES 2" };
-      ViewData["Residencias"] = Residencias;
-      string[] TiposDocumento = { "DNI", "PASAPORTE" };
-      ViewData["TiposDocumento"] = TiposDocumento;
-      string[] Perfiles = { "Administrador", "Residente", "Encagado de Seguridad" };
-      ViewData["Perfiles"] = Perfiles;
-      int idSelected = rowId;
-      return View();
-    }
-    [HttpGet]
-    public IActionResult Barrios(string index, int rowId)
-    {
-      string[] Residencias = { "RES 1", "RES 2" };
-      ViewData["Residencias"] = Residencias;
-      string[] TiposDocumento = { "DNI", "PASAPORTE" };
-      ViewData["TiposDocumento"] = TiposDocumento;
-      string[] Perfiles = { "Administrador", "Residente", "Encagado de Seguridad" };
-      ViewData["Perfiles"] = Perfiles;
-      var idSelected=rowId;
-      return View();
-    }
-
-    public IActionResult VerUsuarios(string index)
-    {
-      return View();
-    }
-
-    public IActionResult Residencias(string index)
-    {
-      ViewData["mundo"] = "probando!";
-      return View();
-    }
-
-
-    public IActionResult Visitas()
-    {
+      // aca hay que hacer..
+      // si esta logueado... muestro la vista, sino
+      // redirijo al login
+      
       return View();
     }
   }
