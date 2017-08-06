5dd510f4d7ca84a2261f4280680ae81f2a7929cb
Pato Perez <pato12p@gmail.com>
2017-08-05 23:06:40 -0300

    Prueba de api

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -10,42 +10,63 @@ namespace PriHood.Controllers
   [Route("api/[controller]")]
   public class UsuariosController : Controller
   {
+
+    private readonly PrihoodContext db;
+
+    public UsuariosController(PrihoodContext context)
+    {
+      db = context;
+    }
+
     // GET api/usuarios
     [HttpGet]
-    public IEnumerable<Usuario> Get()
+    public Object Get()
     {
-      using (PrihoodContext db = new PrihoodContext())
-      {
-        return db.Usuario.ToList();
-      }
+      var usuarios = db.Usuario.ToList();
+
+      return new { error = false, data = usuarios };
     }
 
     // GET api/usuarios/5
     [HttpGet("{id}")]
-    public Usuario Get(int id)
+    public Object Get(int id)
     {
-      using (PrihoodContext db = new PrihoodContext())
-      {
-        return db.Usuario.First(u => u.Id == id);
-      }
+      var usuario = db.Usuario.First(u => u.Id == id);
+
+      return new { error = false, data = usuario };
     }
 
     // POST api/values
     [HttpPost]
-    public void Post([FromBody]string value)
+    public Object Post([FromBody]Usuario usuario)
     {
+      db.Usuario.Add(usuario);
+      db.SaveChanges();
+
+      return new { error = false, data = usuario };
     }
 
     // PUT api/values/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody]string value)
+    public void Put(int id, [FromBody]Usuario usuario)
     {
     }
 
     // DELETE api/values/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public Object Delete(int id)
     {
+      var usuario = db.Usuario.FirstOrDefault(u => u.Id == id);
+
+      if (usuario != null)
+      {
+        db.Usuario.Remove(usuario);
+        db.SaveChanges();
+
+        return new { error = false, data = "Borrado correctamente." };
+      }
+
+      return new { error = true, data = "Error al borrar." };
     }
   }
 }
---
+++ panel_src/app/usuarios/usuario.model.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+  id?: number;
+  avatar: string;
+  email: string;
+  idPerfil: number;
+  nombreUsuario: string;
+  password?: string;
+}
