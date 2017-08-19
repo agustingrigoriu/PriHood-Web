a859b8a3002f5589ce94b4581bd2c5be8601504a
Pato Perez <pato12p@gmail.com>
2017-08-19 14:51:15 -0300

    Agregar crear usuario administrador al crear el barrio

--- Controllers/Api/BarriosController.cs
+++ Controllers/Api/BarriosController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PriHood.Models;
 using Newtonsoft.Json.Linq;
-
+using PriHood.Auth;
 
 namespace PriHood.Controllers
 {
@@ -14,11 +14,13 @@ namespace PriHood.Controllers
   {
 
     private readonly PrihoodContext db;
-
-    public BarriosController(PrihoodContext context)
+    private AuthService auth;
+    public BarriosController(PrihoodContext context, AuthService a)
     {
       db = context;
+      auth = a;
     }
+
 
     [HttpGet]
     public Object Get()
@@ -34,10 +36,55 @@ namespace PriHood.Controllers
     }
 
     [HttpPost]
-    public Object Post([FromBody]Barrio barrio)
+    public Object Post([FromBody]ModeloBarrioAdministrador mba)
     {
-      db.Barrio.Add(barrio);
-      db.SaveChanges();
+      using (var transaction = db.Database.BeginTransaction())
+      {
+        try
+        {
+          db.Barrio.Add(mba.barrio);
+
+          var persona = new Persona();
+          persona.Apellido = mba.usuario.apellido;
+          persona.Nombre = mba.usuario.nombre;
+          persona.FechaNacimiento = mba.usuario.fecha_nacimiento;
+          persona.IdTipoDocumento = mba.usuario.id_tipo_documento;
+          persona.NroDocumento = mba.usuario.numero_documento;
+          persona.TelefonoMovil = mba.usuario.telefono;
+          db.Persona.Add(persona);
+
+          db.SaveChanges();
+
+          var id_persona = persona.Id;
+
+          var perfil = db.Perfil.First(u => u.Descripcion == "RESIDENTE");
+          var usuario = new Usuario();
+
+          usuario.Email = mba.usuario.email;
+          usuario.Password = auth.getHash(mba.usuario.password); // hasheo le password
+          usuario.IdPerfil = perfil.Id; //Tengo q buscar el correspondiente a Residente, no manejarme por ID
+          db.Usuario.Add(usuario);
+
+          db.SaveChanges();
+
+          var id_usuario = usuario.Id;
+
+          var empleado = new Empleado();
+          empleado.IdBarrio = mba.barrio.Id;
+          empleado.IdPersona = id_persona;
+          empleado.IdUsuario = id_usuario;
+          db.Empleado.Add(empleado);
+
+          db.SaveChanges();
+
+          transaction.Commit();
+        }
+        catch (Exception)
+        {
+          transaction.Rollback();
+          return new { error = true, data = "Error" };
+        }
+      }
 
       return new { error = false, data = "ok" };
     }
