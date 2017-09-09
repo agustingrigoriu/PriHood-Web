96cd0dfcd2e1d622202e3ac845ddf38585fa1518
Agustín Gregorieu <agustin.gregorieu@gmail.com>
2017-09-08 23:34:18 -0300

    Incorporo PUT, DELETE y GET (id_amenity) a AmenitiesController

--- Controllers/Api/AmenitiesController.cs
+++ Controllers/Api/AmenitiesController.cs
@@ -43,6 +43,26 @@ namespace PriHood.Controllers
       }
     }
 
+    [HttpPut("{id_amenity}")]
+    public Object ActualizarAmenity(int id_amenity, [FromBody]Amenity amenity)
+    {
+      try
+      {
+        var logueado = HttpContext.Session.Authenticated();
+        var id_barrio = logueado.IdBarrio.Value;
+
+        amenity.Id = id_amenity;
+        db.Amenity.Update(amenity);
+        db.SaveChanges();
+
+        return new { error = false, data = amenity };
+      }
+      catch (Exception err)
+      {
+        return new { error = true, data = err.Message };
+      }
+    }
+
     [HttpGet("{id_tipo_amenity?}")]
     public Object ListarAmenities(int? id_tipo_amenity)
     {
@@ -72,6 +92,66 @@ namespace PriHood.Controllers
         return new { error = true, data = "fail", message = err.Message };
       }
     }
+
+    [HttpGet("amenitie/{id_amenity}")]
+    public Object ObtenerAmenity(int id_amenity)
+    {
+      try
+      {
+        var logueado = HttpContext.Session.Authenticated();
+        var id_barrio = logueado.IdBarrio;
+        var amenity = (
+          from a in db.Amenity
+          join ta in db.TipoAmenity on a.IdTipoAmenity equals ta.Id
+          where a.Id == id_amenity && a.IdBarrio == id_barrio
+          select new
+          {
+            id = a.Id,
+            nombre = a.Nombre,
+            descripcion = a.Descripcion,
+            telefono = a.Telefono,
+            ubicacion = a.Ubicacion,
+            id_tipo_amenity = a.IdTipoAmenity,
+            tipo_amenity = ta.Descripcion
+          }
+        ).FirstOrDefault();
+
+        if (amenity == null) return new { error = true, data = "No existe ese amenity" };
+
+        return new { error = false, data = tipos };
+      }
+      catch (Exception err)
+      {
+        return new { error = true, data = "fail", message = err.Message };
+      }
+    }
+
+    [HttpDelete("{id_amenity}")]
+    public Object EliminarAmenity(int id_amenity)
+    {
+      try
+      {
+        var logueado = HttpContext.Session.Authenticated();
+        var id_barrio = logueado.IdBarrio;
+
+        if (db.Amenity.Where(a => a.Id == id_amenity).Count() > 0)
+        {
+          var amenity = db.Amenity.First(t => t.Id == id_amenity);
+          db.Amenity.Remove(amenity);
+          db.SaveChanges();
+          return new { error = false, data = amenity };
+        }
+
+        return new { error = false, data = "No existe ese amenity" }
+
+      }
+      catch (Exception err)
+      {
+        return new { error = true, data = "fail", message = err.Message };
+      }
+    }
+
+
 
     // [HttpGet("{id_amenity}")]
     // public Object ListarReservasParaAmenity(int id_amenity)
