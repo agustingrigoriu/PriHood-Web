f1699f8fa0bb579094c70cd55d6e37e3568e7a98
Pato Perez <pato12p@gmail.com>
2017-09-12 19:25:17 -0300

    Agregar endpoint para listar amenities con turnos para una fecha determinada

--- Controllers/Api/AmenitiesController.cs
+++ Controllers/Api/AmenitiesController.cs
@@ -152,6 +152,64 @@ namespace PriHood.Controllers
     }
 
 
+    [HttpGet("{id_tipo_amenity}/{fecha:DateTime}")]
+    public Object ListarAmenitiesFechaTurnos(int id_tipo_amenity, DateTime fecha)
+    {
+      try
+      {
+        var logueado = HttpContext.Session.Authenticated();
+        var id_barrio = logueado.IdBarrio.Value;
+        var id_dia = this.fecha2IdDay(fecha);
+        var amenities = (
+          from a in db.Amenity
+          join ta in db.TipoAmenity on a.IdTipoAmenity equals ta.Id
+          where a.IdBarrio == id_barrio && a.IdTipoAmenity == id_tipo_amenity
+          where (
+            from t in db.Turno
+            where t.IdDiaSemana == id_dia && t.IdAmenity == a.Id
+            select t
+          ).Count() > 0
+          select new
+          {
+            id = a.Id,
+            nombre = a.Nombre,
+            descripcion = a.Descripcion,
+            telefono = a.Telefono,
+            ubicacion = a.Ubicacion,
+            tipo_amenity = ta.Descripcion
+          }
+        ).ToList();
+
+        return new { error = false, data = amenities };
+
+      }
+      catch (Exception err)
+      {
+        return new { error = true, data = "fail", message = err.Message };
+      }
+    }
+
+    private int fecha2IdDay(DateTime fecha)
+    {
+      switch (fecha.DayOfWeek)
+      {
+        case DayOfWeek.Monday:
+          return 1;
+        case DayOfWeek.Tuesday:
+          return 2;
+        case DayOfWeek.Wednesday:
+          return 3;
+        case DayOfWeek.Thursday:
+          return 4;
+        case DayOfWeek.Friday:
+          return 5;
+        case DayOfWeek.Saturday:
+          return 6;
+        case DayOfWeek.Sunday:
+        default:
+          return 7;
+      }
+    }
 
     // [HttpGet("{id_amenity}")]
     // public Object ListarReservasParaAmenity(int id_amenity)
