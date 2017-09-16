f2cffa212a42510ffc277f4f3e65ea41d024c0de
Agustín Gregorieu <agustin.gregorieu@gmail.com>
2017-09-16 20:16:18 -0300

    Prueba dashboard

--- panel_src/app/home/home.component.ts
+++ panel_src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Usuario } from '../usuarios/usuario.model';
 
 
 @Component({
@@ -7,8 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(protected LoginService: LoginService) { }
   title = 'Home';
   lat: number = -31.335335;
   lng: number = -64.303113;
+  mensaje: string;
+  headerClass: string = " ";
+
+  public usuario: Usuario = {
+    email: '',
+    idPerfil: -1,
+    nombre: '',
+    apellido: '',
+    perfil: '',
+    barrio: ''
+  };
+
+  getUsuario() {
+    this.LoginService.getUserLogin().then(response => {
+      if (response.error) {
+        this.mensaje = 'No se pudo crear la residencia.';
+        this.headerClass = "alert-danger";
+      } else {
+        this.usuario = response.data;
+        this.mensaje = 'Se creo correctamente.';
+        this.headerClass = "alert-success";
+      }
+    });
+  }
+
+
+  ngOnInit(): void {
+    this.getUsuario();
+  }
+
 }
