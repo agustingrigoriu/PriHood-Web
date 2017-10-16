bc0391ddbbfec7842f8f433ccc3b95732b15cf41
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2017-10-16 16:36:31 -0300

    Actualizo modelos para incorporación de nuevos campos latitud y longitud

--- Models/Barrio.cs
+++ Models/Barrio.cs
@@ -14,6 +14,8 @@ namespace PriHood.Models
         }
 
         public int Id { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
         public string Nombre { get; set; }
         public string Ubicacion { get; set; }
 
