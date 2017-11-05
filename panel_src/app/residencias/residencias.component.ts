9695c5d289743d7fb27600e8624db3fff0b05a58
@valdiviabelen <elianabelen.valdivia@gmail.com>
2017-11-05 17:04:38 -0300

    spinner residencia

--- panel_src/app/residencias/residencias.component.ts
+++ panel_src/app/residencias/residencias.component.ts
@@ -19,6 +19,7 @@ export class ResidenciaComponent implements OnInit {
   constructor(protected ResidenciasService: ResidenciasService, private notificaciones: NotificationsService, private confirmacion: ConfirmationService) { }
 
   residencias: Residencia[] = [];
+  cargando: boolean;
   residencia: Residencia = {
     nombre: '',
     ubicacion: ''
@@ -26,7 +27,9 @@ export class ResidenciaComponent implements OnInit {
   residenciaSeleccionada: Residencia;
 
   agregarResidencia(residencia: Residencia, form: NgForm) {
+    this.cargando = true;
     this.ResidenciasService.crearResidencia(residencia).then(response => {
+      this.cargando = false;
       if (response.error) {
         this.notificaciones.error("Error", "No se pudo crear la residencia");
       } else {
@@ -38,7 +41,9 @@ export class ResidenciaComponent implements OnInit {
   }
 
   borrarResidencia(res: Residencia) {
+    this.cargando = true;
     this.ResidenciasService.deleteResidencia(res.id).then(response => {
+      this.cargando = false;
       if (response.error) {
         this.notificaciones.error("Error", "No se pudo borrar la residencia");
       } else {
@@ -53,7 +58,9 @@ export class ResidenciaComponent implements OnInit {
   }
 
   modificarResidencia(residencia: Residencia) {
+    this.cargando = true;
     this.ResidenciasService.updateResidencia(residencia.id, residencia).then(response => {
+      this.cargando = false;
       if (response.error) {
         this.notificaciones.error("Error", "No se pudo modificar la residencia");
       } else {
@@ -64,11 +71,13 @@ export class ResidenciaComponent implements OnInit {
   }
 
   actualizar() {
+    this.cargando = true;
     this.ResidenciasService.getAllResidencias().then(response => {
       if (response.error) {
         alert('No se pudo cargar las residencias.');
       } else {
         this.residencias = response.data;
+        this.cargando = false;
       }
     });
   }
