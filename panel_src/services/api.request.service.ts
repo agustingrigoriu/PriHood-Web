d3ab5f1b4d638ee0638b9a49a61d2c3bf0bdf949
Pato Perez <pato12p@gmail.com>
2017-11-05 20:01:08 -0300

    Redireccionar al login cuando estas el usuario deslogueado

--- panel_src/services/api.request.service.ts
+++ panel_src/services/api.request.service.ts
@@ -10,19 +10,19 @@ export class ApiRequestService {
   public BASE_URL = 'http://localhost:5000';
 
   post<T>(api, data?, params?: HttpParams): Promise<Request<T>> {
-    return this.http.post<Request<T>>(`${this.BASE_URL}/api/${api}`, data, { params }).toPromise();
+    return this.http.post<Request<T>>(`${this.BASE_URL}/api/${api}`, data, { params }).toPromise().catch(this.handlerLogout.bind(this));
   }
 
   put<T>(api, data?, params?: HttpParams): Promise<Request<T>> {
-    return this.http.put<Request<T>>(`${this.BASE_URL}/api/${api}`, data, { params }).toPromise();
+    return this.http.put<Request<T>>(`${this.BASE_URL}/api/${api}`, data, { params }).toPromise().catch(this.handlerLogout.bind(this));
   }
 
   delete<T>(api, params?: HttpParams): Promise<Request<T>> {
-    return this.http.delete<Request<T>>(`${this.BASE_URL}/api/${api}`, { params }).toPromise();
+    return this.http.delete<Request<T>>(`${this.BASE_URL}/api/${api}`, { params }).toPromise().catch(this.handlerLogout.bind(this));
   }
 
   get<T>(api, params?: HttpParams): Promise<Request<T>> {
-    return this.http.get<Request<T>>(`${this.BASE_URL}/api/${api}`, { params }).toPromise();
+    return this.http.get<Request<T>>(`${this.BASE_URL}/api/${api}`, { params }).toPromise().catch(this.handlerLogout.bind(this));
   }
 
   upload<T>(api, data, file: File, input = 'file'): Promise<Request<T>> {
@@ -35,6 +35,14 @@ export class ApiRequestService {
     form.append(input, file, file.name);
 
     return this.http.post<Request<T>>(`${this.BASE_URL}/api/${api}`, form).toPromise();
+  }
+
+  handlerLogout(response) {
+    if(response.status === 401) {
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(response);
   }
 }
 
