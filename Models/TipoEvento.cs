bc8661330a6eca9c8750cafbf20d3a4ffab06261
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2017-11-05 15:12:27 -0300

    Actualizo BD para incorporar img a tipo de evento y actualizo modelos

--- Models/TipoEvento.cs
+++ Models/TipoEvento.cs
@@ -12,6 +12,7 @@ namespace PriHood.Models
 
         public int Id { get; set; }
         public string Descripcion { get; set; }
+        public string Imagen { get; set; }
 
         public virtual ICollection<Eventos> Eventos { get; set; }
     }
